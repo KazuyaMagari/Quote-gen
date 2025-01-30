2ed496df8a2460f13313e987b966f546693ffdf9
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-01-30 02:10:21 -0500

    add server operations

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-const corsOption = {origin: "http://localhost:5174",  methods: "GET,POST", // Allow GET and POST requests
+const corsOption = {origin: "http://localhost:5173",  methods: "GET,POST", // Allow GET and POST requests
     allowedHeaders: "Content-Type,Authorization",   };
 const Quote = require("../server/schemas/QuoteShemas");
 const mongoose = require("mongoose");
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); // 
 app.use(cors(corsOption));
 mongoose.connect('mongodb://127.0.0.1:27017/quotes')
 .then(() => {
@@ -19,6 +21,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/quotes')
 app.get('/', async(req, res) => {
     const response = await Quote.find();
     res.json(response)
+})
+
+app.post("/favorites", async (req, res) => {
+    const {quote} = req.body;
+    const quoteRes = await Quote.find({quote: quote})
+    console.log(quoteRes)
 })
 
 app.listen(3001, (req, res) =>{
